80c8e69e347e23de773b0ba5be8c63bd56cb1138
MohammadMosaAghaJahanmal <iamceayber@gmail.com>
2023-01-31 23:27:59 +0430

    eco vission mission about with server

--- src/Containers/E_VissionMission/index.js
+++ src/Containers/E_VissionMission/index.js
@@ -1,64 +1,80 @@
 import React, {useEffect, useState} from "react";
 import styles from './style.module.css';
 import SmallHero from '../../Components/SmallHero';
-import HeroImage from '../../Assets/lab.jpg';
 import language from '../../localization';
 import Title from '../../Components/Title';
 import Text from "../../Components/Text";
 import SideBar from "../../Components/SidaBar";
-
+import useStore from "../../store/store";
+import serverPath from '../../utils/serverPath';
 const E_VissionMission = (props) =>
 {
+  const [globalState] = useStore();
+  const {heros, cevissions, cemissions, ceabouts} = globalState;
+  const [visMis, setVisMis] = useState({
+    vission: {},
+    mission: {},
+    about: {}
+  });
+
+  useEffect(() => {
+    let vis = cevissions.find(v => v.type === 'eco');
+    let mis = cemissions.find(v => v.type === 'eco');
+    let about = ceabouts.find(v => v.type === 'eco');
+    setVisMis(prev => ({
+      vission: vis || {},
+      mission: mis || {},
+      about: about || {},
+    }));
+  }, []);
+  const myHero = new URL(serverPath(heros?.find(hero => hero.type === "vission_mission")?.imagePath || "")).href;
+
   const isRTL = (language.getLanguage() === 'ps');
 
   return (
     <div className={styles.vissionMission}>
-      <SmallHero title={language.a_vission_mission} image={HeroImage}  bgAnimation={true}/>
+      <SmallHero title={language.a_vission_mission} image={myHero}  bgAnimation={true}/>
       <div className={[styles.vmw, "w-controller"].join(" ")}>
+      {
+          (visMis.vission?.title && visMis.mission?.title && visMis.about?.description) ?
         <div className={styles.wrapper}>
           <div className={styles.vm}>
             <div className={styles.vmCard}>
-              <Title title="Vission" className={styles.title}/>
-              <Text text="
-                To be a recognized university in the country 
-                and region for quality teaching, research and learning, 
-                strong management system for nurturing a generation disciplined with national values brightening future of the country.
-                " 
+              <Title title={visMis.vission[isRTL ? "pTitle" : "title"]} className={styles.title}/>
+              <Text text={visMis.vission[isRTL ? "pDescription" : "description"]}
                 className={styles.text}
               />
             </div>
             <div className={styles.vmCard}>
-              <Title title="Mission" className={styles.title} />
-              <Text text="
-                Saba University serves the nation and region through providing, 
-                retaining, and practicing value-based knowledge, 
-                skills, research and producing strong academic cadres known for their best Islamic, 
-                national, and social characters.
-                "
+              <Title title={visMis.mission[isRTL ? "pTitle" : "title"]} className={styles.title} />
+              <Text text={visMis.mission[isRTL ? "pDescription" : "description"]}
                 className={styles.text}
               />
             </div>
             <div className={styles.achive}>
               <Title 
-                  title="No Title From Backend Just Text Editor"
+                  title={language.a_about}
                   className={[styles.chTitle, styles.title].join(" ")}
                   />
               <div className={styles.textData}>
                 <p>
-                  Some Text From backend
+                  {visMis.about[isRTL ? "pDescription" : "description"]}
                 </p>
               </div>
             </div>
           </div>
           <SideBar
               links={[
-                {name: language.a_vission_mission, link: "/academic/e_vission_mission"},
-                {name: language.a_curriculum, link: "/academic/e_curriculum"},
-                {name: language.a_organizational_structure, link: "/academic/e_organizational_structure"},
-                {name: language.a_aggrements, link: "/academic/e_aggrements"},
+                {name: language.a_vission_mission, link: "/academic/c_vission_mission"},
+                {name: language.a_curriculum, link: "/academic/c_curriculum"},
+                {name: language.a_organizational_structure, link: "/academic/c_organizational_structure"},
+                {name: language.a_aggrements, link: "/academic/c_aggrements"},
               ]}
             />
         </div>
+        : 
+        <p className="msg" style={{padding: "50px 0"}}>{language.nothing_to_show}</p>
+        }
       </div>
     </div>
   )
