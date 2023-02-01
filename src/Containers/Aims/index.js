2472199be763728b759966a3b7430bba8be3f795
MohammadMosaAghaJahanmal <iamceayber@gmail.com>
2023-02-01 16:00:39 +0430

    aim with server

--- src/Containers/Aims/index.js
+++ src/Containers/Aims/index.js
@@ -1,22 +1,31 @@
 import React, {useEffect, useState} from "react";
 import styles from './style.module.css';
 import SmallHero from '../../Components/SmallHero';
-import HeroImage from '../../Assets/polic.jpg';
 import language from '../../localization';
 import Title from '../../Components/Title';
 import Text from "../../Components/Text";
 import SideBar from "../../Components/SidaBar";
+import serverPath from "../../utils/serverPath";
+import useStore from "../../store/store";
 
 const Aims = (props) =>
 {
 
+  const [globalState] = useStore();
+
+  const {heros, aaims} = globalState;
+
+  const aim = aaims[0];
+
   const isRTL = (language.getLanguage() === 'ps');
+  const myHero = new URL(serverPath(heros?.find(hero => hero.type === "a_aims")?.imagePath || "")).href;
 
   return (
     <div className={styles.container}>
-      <SmallHero title={language.a_aims} image={HeroImage} style={{color: "#0080d6", textShadow: "0 0 2px white"}}  bgAnimation={true}/>
+      <SmallHero title={language.a_aims} image={myHero}  bgAnimation={true}/>
       <div className={[styles.cw, "w-controller"].join(" ")}>
         <div className={styles.contentWrapper}>
+        {aaims?.length > 0 ?
           <div className={styles.wrapper}>
             <div className={styles.content}>
               <Title
@@ -25,8 +34,7 @@ const Aims = (props) =>
                   />
               <Text className={styles.text}>
                 <div className={styles.textData}>
-                  <p>Saba University was established in 2011 to provide quality higher education that Respond to the needs of society and the labor market. Since then, we have produced More than 3200 graduates collectively from journalism, civil engineering, economics, Sharia, law, and political sciences.</p>
-                  <p>Saba University was established in 2011 to provide quality higher education that Respond to the needs of society and the labor market. Since then, we have produced More than 3200 graduates collectively from journalism, civil engineering, economics, Sharia, law, and political sciences.</p>
+                  {aim[isRTL ? "pDescription" : "description"]}
                 </div>
               </Text>
             </div>
@@ -42,6 +50,9 @@ const Aims = (props) =>
               ]}
               />
           </div>
+          :
+          <p className="msg">{language.nothing_to_show}</p>
+          }
         </div>
       </div>
     </div>
