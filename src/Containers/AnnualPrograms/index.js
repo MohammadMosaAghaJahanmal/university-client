c553f9a11594a4c4cd86d711315a51f085a40116
MohammadMosaAghaJahanmal <iamceayber@gmail.com>
2023-02-01 17:04:34 +0430

    annual programs with server

--- src/Containers/AnnualPrograms/index.js
+++ src/Containers/AnnualPrograms/index.js
@@ -6,27 +6,36 @@ import language from '../../localization';
 import Title from '../../Components/Title';
 import Text from "../../Components/Text";
 import SideBar from "../../Components/SidaBar";
+import serverPath from "../../utils/serverPath";
+import useStore from "../../store/store";
 
 const AnnualPrograms = (props) =>
 {
 
+  const [globalState] = useStore();
+
+  const {heros, aannualprograms} = globalState;
+
+  const anualPrograms = aannualprograms[0];
+
   const isRTL = (language.getLanguage() === 'ps');
+  const myHero = new URL(serverPath(heros?.find(hero => hero.type === "a_annual_program_monitoring")?.imagePath || "")).href;
 
   return (
     <div className={styles.container}>
-      <SmallHero title={language.a_annual_program_monitoring} image={HeroImage} style={{color: "#0080d6", textShadow: "0 0 2px white"}}  bgAnimation={true}/>
+      <SmallHero title={language.a_annual_program_monitoring} image={myHero} style={{color: "#0080d6", textShadow: "0 0 2px white"}}  bgAnimation={true}/>
       <div className={[styles.cw, "w-controller"].join(" ")}>
         <div className={styles.contentWrapper}>
+        {aannualprograms?.length > 0 ?
           <div className={styles.wrapper}>
             <div className={styles.content}>
               <Title
-                  title="Title From Backend"
+                  title={anualPrograms[isRTL ? "pTitle" : "title"]}
                   className={[styles.chTitle].join(" ")}
                   />
               <Text className={styles.text}>
                 <div className={styles.textData}>
-                  <p>Saba University was established in 2011 to provide quality higher education that Respond to the needs of society and the labor market. Since then, we have produced More than 3200 graduates collectively from journalism, civil engineering, economics, Sharia, law, and political sciences.</p>
-                  <p>Saba University was established in 2011 to provide quality higher education that Respond to the needs of society and the labor market. Since then, we have produced More than 3200 graduates collectively from journalism, civil engineering, economics, Sharia, law, and political sciences.</p>
+                {anualPrograms[isRTL ? "pDescription" : "description"]}
                 </div>
               </Text>
             </div>
@@ -42,6 +51,9 @@ const AnnualPrograms = (props) =>
               ]}
               />
           </div>
+          :
+          <p className="msg">{language.nothing_to_show}</p>
+          }
         </div>
       </div>
     </div>
