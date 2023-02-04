1c2a0651c8bd5096a1421faaa75f6c8376e98e4f
MohammadMosaAghaJahanmal <iamceayber@gmail.com>
2023-02-04 21:57:13 +0430

    connected with server

--- src/Containers/MigrationPolicy/index.js
+++ src/Containers/MigrationPolicy/index.js
@@ -1,34 +1,43 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import styles from './style.module.css';
 import SmallHero from '../../Components/SmallHero';
-import HeroImage from '../../Assets/polic.jpg';
 import language from '../../localization';
 import Title from '../../Components/Title';
 import Text from "../../Components/Text";
 import SideBar from "../../Components/SidaBar";
-
+import serverPath from "../../utils/serverPath";
+import useStore from "../../store/store";
 const MigrationPolicy = (props) =>
 {
+  const [globalState] = useStore();
+
+  const {heros, migrationpolicies : mps} = globalState;
+
 
   const isRTL = (language.getLanguage() === 'ps');
+  const myHero = new URL(serverPath(heros?.find(hero => hero.type === "migration_policy")?.imagePath || "")).href;
 
   return (
     <div className={styles.container}>
-      <SmallHero title={language.migration_policy} image={HeroImage} style={{color: "#0080d6", textShadow: "0 0 2px white"}}  bgAnimation={true}/>
+      <SmallHero title={language.migration_policy} image={myHero} style={{color: "#0080d6", textShadow: "0 0 2px white"}}  bgAnimation={true}/>
       <div className={[styles.haw, "w-controller"].join(" ")}>
         <div className={styles.contentWrapper}>
+        {mps?.length > 0 ?
           <div className={styles.wrapper}>
             <div className={styles.achive}>
-              <Title 
-                  title="TITLE FROM BACKEND"
-                  className={[styles.chTitle].join(" ")}
-                  />
-              <Text className={styles.text}>
-                <div className={styles.textData}>
-                  <p>Saba University was established in 2011 to provide quality higher education that Respond to the needs of society and the labor market. Since then, we have produced More than 3200 graduates collectively from journalism, civil engineering, economics, Sharia, law, and political sciences.</p>
-                  <p>Saba University was established in 2011 to provide quality higher education that Respond to the needs of society and the labor market. Since then, we have produced More than 3200 graduates collectively from journalism, civil engineering, economics, Sharia, law, and political sciences.</p>
-                </div>
-              </Text>
+            {mps.map(mp => (
+              <div key={mp._id}>
+                <Title 
+                    title={mp[isRTL ? "pTitle" : "title"]}
+                    className={[styles.chTitle].join(" ")}
+                    />
+                <Text className={styles.text}>
+                  <div className={styles.textData}>
+                    {mp[isRTL ? "pDescription" : "description"]}
+                  </div>
+                </Text>
+              </div>
+              ))}
             </div>
             <SideBar
               links={[
@@ -42,6 +51,9 @@ const MigrationPolicy = (props) =>
               ]}
               />
           </div>
+        :
+        <p className="msg">{language.nothing_to_show}</p>
+        }
         </div>
       </div>
     </div>
