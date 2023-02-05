d55524db481d745c8259b0b1f6e56bc8cf996b78
MohammadMosaAghaJahanmal <iamceayber@gmail.com>
2023-02-05 15:19:27 +0430

    registeration connected with server

--- src/Containers/OnlineAdmission/index.js
+++ src/Containers/OnlineAdmission/index.js
@@ -1,14 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, { useState }  from "react";
 import styles from './style.module.css';
 import SmallHero from '../../Components/SmallHero';
 import HeroImage from '../../Assets/admission.jpg';
-import Input from '../../Components/Input';
-import Button from "../../Components/Button";
-import { FaFacebookF, FaMapMarkedAlt, FaYoutube } from "react-icons/fa";
 import languages from "../../localization";
 import MaterialInput from "../../Components/MaterialInput";
 import { provinces } from '../../Constants'
-
+import SweetAlert from '../../Components/SweetAlert';
+import serverPath from "../../utils/serverPath";
+import createValidForm from '../../utils/createValidForm';
 const OnlineAdmission = (props) =>
 {
 
@@ -22,8 +21,57 @@ const OnlineAdmission = (props) =>
 
   for (;prevYears <= currentYear; prevYears++) {
     years.unshift(prevYears)
+  };
+
+  const initialState = {
+    fullName: {value: "", required: true},
+    pFullName: {value: "", required: true},
+    fatherName: {value: "", required: true},
+    pFatherName: {value: "", required: true},
+    province: {value: "Kandahar", required: true},
+    pProvince: {value: "Kandahar", required: true},
+    phone: {value: "", required: true},
+    graduationYear: {value: years[0], required: true},
+    highSchool: {value: "", required: true},
+    pHighSchool: {value: "", required: true},
+    email: {value: "", required: false},
+  };
+  
+  const [admission, setAdmission] = useState({...initialState});
+
+  const onChange = (type, value) =>
+  {
+    setAdmission(prev => ({...prev, [type]: {...prev[type], value}}))
   }
 
+  const submitHandler = async () =>
+  {
+    let formData = {};
+    for (const fieldKey in admission) {
+      if (Object.hasOwnProperty.call(admission, fieldKey)) {
+        const value = admission[fieldKey];
+        if(value.value.length <= 0 && value.required)
+          return SweetAlert("error", fieldKey.toUpperCase() + " is not allowed to by empty!");
+          formData[fieldKey] = value.value;
+      }
+    }
+    const validForm = createValidForm(formData);
+    try {
+      
+      const {status, message, data} = await (await fetch(serverPath('/student'), {
+        method: "POST",
+        body: validForm
+      })).json()
+      if(status === "failure")
+        return SweetAlert('error', message);
+
+      SweetAlert("success", "Successfully registered!");
+      setAdmission({...initialState});
+      console.log(data)
+    } catch (error) {
+      return SweetAlert('error', error.message);
+    }
+  }
   return (
     <div className={styles.oa}>
       <SmallHero title={languages.online_admission} image={HeroImage} isRTL={isRTL} style={{backgroundPosition: "bottom"}}/>
@@ -38,7 +86,11 @@ const OnlineAdmission = (props) =>
                   label={"Full Name *"}
                   placeholder={"Name *"}
                   id="fullname"
-                  onChange={(e) => {}}
+                  onChange={(e) => {
+                    onChange("fullName", e.target.value)
+                    onChange("pFullName", e.target.value)
+                  }}
+                  value={admission.fullName.value}
                   />
               </div>
               <div data-aos="fade-right" data-aos-delay={200}>
@@ -46,7 +98,11 @@ const OnlineAdmission = (props) =>
                   label={"Father Name *"}
                   placeholder={"F/Name *"}
                   id="fname"
-                  onChange={(e) => {}}
+                  onChange={(e) => {
+                    onChange("fatherName", e.target.value)
+                    onChange("pFatherName", e.target.value)
+                  }}
+                  value={admission.fatherName.value}
                   />
               </div>
               <div data-aos="fade-right" data-aos-delay={400}>
@@ -56,8 +112,11 @@ const OnlineAdmission = (props) =>
                   id="gyear"
                   type="select"
                   options={years}
-                  select={currentYear}
-                  onChange={(e) => {}}
+                  select={admission.graduationYear.value}
+                  onChange={(e) => {
+                    onChange("graduationYear", e.target.value)
+                  }}
+                  value={admission.graduationYear.value}
                   />
               </div>
               <div data-aos="fade-right" data-aos-delay={600}>
@@ -65,7 +124,11 @@ const OnlineAdmission = (props) =>
                   label={"High School Name *"}
                   placeholder={"School Name *"}
                   id="sname"
-                  onChange={(e) => {}}
+                  onChange={(e) => {
+                    onChange("highSchool", e.target.value)
+                    onChange("pHighSchool", e.target.value)
+                  }}
+                  value={admission.highSchool.value}
                   />
               </div>
               <div data-aos="fade-right" data-aos-delay={800}>
@@ -73,7 +136,10 @@ const OnlineAdmission = (props) =>
                   label={"Phone Number *"}
                   placeholder={"P/Number *"}
                   id="pname"
-                  onChange={(e) => {}}
+                  onChange={(e) => {
+                    onChange("phone", e.target.value)
+                  }}
+                  value={admission.phone.value}
                   />
               </div>
               <div data-aos="fade-right" data-aos-delay={1000}>
@@ -83,8 +149,13 @@ const OnlineAdmission = (props) =>
                   id="province"
                   type="select"
                   options={provinces}
-                  select={"Kandahar"}
-                  onChange={(e) => {}}
+                  select={admission.province.value}
+                  onChange={(e) => {
+                    console.log(e.target.value)
+                    onChange("province", e.target.value)
+                    onChange("pProvince", e.target.value)
+                  }}
+                  value={admission.province.value}
                   />
               </div>
               <div data-aos="fade-right" data-aos-delay={1200}>
@@ -92,11 +163,14 @@ const OnlineAdmission = (props) =>
                   label={"Email Address"}
                   placeholder={"Email"}
                   id="email"
-                  onChange={(e) => {}}
+                  onChange={(e) => {
+                    onChange("email", e.target.value)
+                  }}
+                  value={admission.email.value}
                   />
               </div>
             <div className={styles.oaButton} data-aos="fade-up" data-aos-delay={1400}>
-              <button>
+              <button onClick={submitHandler}>
                 Register
               </button>
             </div>
