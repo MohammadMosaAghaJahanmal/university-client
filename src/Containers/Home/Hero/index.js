2cabfd0eb27c6d486ba3265ced31f796ac295787
MohammadMosaAghaJahanmal <iamceayber@gmail.com>
2023-02-11 21:48:49 +0430

    home screen hero with server

--- src/Containers/Home/Hero/index.js
+++ src/Containers/Home/Hero/index.js
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from "react";
 import styles from './style.module.css';
-
+import useStore from "../../../store/store";
+import serverPath from '../../../utils/serverPath'
 const Hero = (props) =>
 {
+  const [globalState, dispatch] = useStore();
+  const {heros} = globalState;
 
+  const myHero = new URL(serverPath(heros?.find(hero => hero.type === "home")?.imagePath || "")).href;
   return (
-    <div className={styles.hero}>
+    <div className={styles.hero} style={{backgroundImage: `url(${myHero})`}}>
       <div className={styles.leftSider}>
           <p className={styles.headText} data-aos="fade-down-right" data-aos-delay={1000}>
             Higher Education with national
