7f6301c299014a1991101d0ce328c7a0f2d9ff2a
MohammadMosaAghaJahanmal <iamceayber@gmail.com>
2023-12-04 18:44:51 +0430

    title changed

--- src/Containers/PlanedResearche/index.js
+++ src/Containers/PlanedResearche/index.js
@@ -18,7 +18,7 @@ const PlanedResearche = (props) =>
 
   return (
     <div className={styles.container}>
-      <SmallHero title={language.strategic_aims} image={myHero}  bgAnimation={true}/>
+      <SmallHero title={language.planed_researche} image={myHero}  bgAnimation={true}/>
       <div className={[styles.cw, "w-controller"].join(" ")}>
         <div className={styles.contentWrapper}>
         {planedresearches?.length > 0 ?
